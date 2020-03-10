f6483eb1471cf3310d27c89d0f7ebebb1b3fef01
Satyan Jacquens <satyan.jacquens@fabernovel.com>
2020-03-10 14:12:50 +0100

    Use gradle kotlin build script

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+buildscript {
+    repositories {
+        google()
+        jcenter()
+    }
+
+    dependencies {
+        classpath("com.android.tools.build", "gradle", "3.6.1")
+        classpath("org.jetbrains.kotlin", "kotlin-gradle-plugin", "1.3.70")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+    }
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.buildDir)
+}
--- sample-app/build.gradle.kts
+++ sample-app/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.config.KotlinCompilerVersion
 
 plugins {
-    id("com.android.library")
+    id("com.android.application")
     kotlin("android")
     kotlin("android.extensions")
 }
@@ -13,6 +13,7 @@ android {
     viewBinding.isEnabled = true
 
     defaultConfig {
+        applicationId = "com.stateful.sample_app"
         minSdkVersion(21)
         targetSdkVersion(29)
         versionCode = 1
@@ -31,11 +32,6 @@ android {
         }
     }
 
-    sourceSets {
-        getByName("main").java.srcDirs("src/main/ktx")
-        getByName("main").res.srcDirs("src/main/res-public")
-    }
-
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -44,19 +40,23 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
 }
 
 dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
 
+    implementation(project(":lib"))
+
     implementation(kotlin("stdlib-jdk8", KotlinCompilerVersion.VERSION))
+    implementation("androidx.appcompat:appcompat:1.1.0")
+    implementation("androidx.core:core-ktx:1.2.0")
+    implementation("androidx.constraintlayout:constraintlayout:1.1.3")
+    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.2.0")
 
-    implementation("androidx.appcompat", "appcompat", "1.1.0")
-    implementation("androidx.constraintlayout", "constraintlayout", "1.1.3")
-    implementation("androidx.core", "core-ktx", "1.2.0")
+    testImplementation("junit:junit:4.13")
 
-
-    testImplementation("junit", "junit", "4.12")
-    androidTestImplementation("androidx.test.ext", "junit", "1.1.1")
-    androidTestImplementation("androidx.test.espresso", "espresso-core", "3.2.0")
+    androidTestImplementation("androidx.test.ext:junit:1.1.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.2.0")
 }
