d17d1ef519be555f3362991fb4dc1adc07bfea0a
Satyan Jacquens <satyan.jacquens@fabernovel.com>
2020-04-30 18:14:23 +0200

    Use robot pattern for tests

--- sample-app/src/test/java/com/fabernovel/statefullayout/sample/ui/main/MainFragmentTest.kt
+++ sample-app/src/test/java/com/fabernovel/statefullayout/sample/ui/main/MainFragmentTest.kt
@@ -1,17 +1,7 @@
 package com.fabernovel.statefullayout.sample.ui.main
 
 import android.os.Build
-import androidx.fragment.app.testing.FragmentScenario
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.Visibility.GONE
-import androidx.test.espresso.matcher.ViewMatchers.Visibility.VISIBLE
-import androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import com.fabernovel.statefullayout.sample.R
-import com.fabernovel.statefullayout.utils.withStatefulLayoutStateId
-import org.junit.Before
+import com.fabernovel.statefullayout.robot.`on main fragment`
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
@@ -20,66 +10,35 @@ import org.robolectric.annotation.Config
 @RunWith(RobolectricTestRunner::class)
 @Config(sdk = [Build.VERSION_CODES.P])
 class MainFragmentTest {
-    private lateinit var scenario: FragmentScenario<MainFragment>
-
-    @Before
-    fun setUp() {
-        scenario = FragmentScenario.launchInContainer(MainFragment::class.java)
-    }
-
     @Test
     fun `when pressing show error, error state should be displayed`() {
-        onView(withId(R.id.errorButton)).perform(click())
-
-        onView(withStatefulLayoutStateId(R.id.stateContent))
-            .check(matches(withEffectiveVisibility(GONE)))
-        onView(withStatefulLayoutStateId(R.id.stateLoading))
-            .check(matches(withEffectiveVisibility(GONE)))
-        onView(withStatefulLayoutStateId(R.id.stateCustom))
-            .check(matches(withEffectiveVisibility(GONE)))
-        onView(withStatefulLayoutStateId(R.id.stateError))
-            .check(matches(withEffectiveVisibility(VISIBLE)))
+        `on main fragment` {
+            clickOnErrorButton()
+            isErrorStateDisplayed()
+        }
     }
 
     @Test
     fun `when pressing show loading, loading state should be displayed`() {
-        onView(withId(R.id.loadingButton)).perform(click())
-
-        onView(withStatefulLayoutStateId(R.id.stateContent))
-            .check(matches(withEffectiveVisibility(GONE)))
-        onView(withStatefulLayoutStateId(R.id.stateError))
-            .check(matches(withEffectiveVisibility(GONE)))
-        onView(withStatefulLayoutStateId(R.id.stateCustom))
-            .check(matches(withEffectiveVisibility(GONE)))
-        onView(withStatefulLayoutStateId(R.id.stateLoading))
-            .check(matches(withEffectiveVisibility(VISIBLE)))
+        `on main fragment` {
+            clickOnLoadingButton()
+            isLoadingStateDisplayed()
+        }
     }
 
     @Test
     fun `when pressing show content, content state should be displayed`() {
-        onView(withId(R.id.contentButton)).perform(click())
-
-        onView(withStatefulLayoutStateId(R.id.stateLoading))
-            .check(matches(withEffectiveVisibility(GONE)))
-        onView(withStatefulLayoutStateId(R.id.stateError))
-            .check(matches(withEffectiveVisibility(GONE)))
-        onView(withStatefulLayoutStateId(R.id.stateCustom))
-            .check(matches(withEffectiveVisibility(GONE)))
-        onView(withStatefulLayoutStateId(R.id.stateContent))
-            .check(matches(withEffectiveVisibility(VISIBLE)))
+        `on main fragment` {
+            clickOnContentButton()
+            isContentStateDisplayed()
+        }
     }
 
     @Test
     fun `when pressing show custom, custom state should be displayed`() {
-        onView(withId(R.id.customButton)).perform(click())
-
-        onView(withStatefulLayoutStateId(R.id.stateLoading))
-            .check(matches(withEffectiveVisibility(GONE)))
-        onView(withStatefulLayoutStateId(R.id.stateError))
-            .check(matches(withEffectiveVisibility(GONE)))
-        onView(withStatefulLayoutStateId(R.id.stateContent))
-            .check(matches(withEffectiveVisibility(GONE)))
-        onView(withStatefulLayoutStateId(R.id.stateCustom))
-            .check(matches(withEffectiveVisibility(VISIBLE)))
+        `on main fragment` {
+            clickOnCustomButton()
+            isCustomStateDisplayed()
+        }
     }
 }
