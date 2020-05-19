4f2b94ed585d2b57067dd7b434c7322d4889d2d9
s.jcqs <satyan.jacquens@fabernovel.com>
2020-05-19 09:45:35 +0200

    Test if duplicated states view is fixed

---
+++ sample-app/src/main/java/com/fabernovel/statefullayout/sample/ui/override/OverrideDefaultFragment.kt
@@ -0,0 +1,6 @@
+package com.fabernovel.statefullayout.sample.ui.override
+
+import androidx.fragment.app.Fragment
+import com.fabernovel.statefullayout.sample.R
+
+class OverrideDefaultFragment : Fragment(R.layout.override_default_fragment)
