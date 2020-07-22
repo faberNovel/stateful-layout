6d225dbe5ec20971fcf000cf753400e6af249d72
s.jcqs <satyan.jacquens@fabernovel.com>
2020-07-22 11:50:40 +0200

    Fix coding style issues

--- lib/src/main/java/com/fabernovel/statefullayout/transitions/CallbackStateTransition.kt
+++ lib/src/main/java/com/fabernovel/statefullayout/transitions/CallbackStateTransition.kt
@@ -7,14 +7,14 @@ internal typealias StateTransitionCallback = (StateTransition, StateTransitionLi
 internal class CallbackStateTransition(
     private val startTransition: StateTransitionCallback,
     private val onCancel: () -> Unit,
-    private val transitionListenerHandler: TransitionListenerHandler = DefaultTransitionListenerHandler()
-) : StateTransition, TransitionListenerHandler by transitionListenerHandler {
+    private val handler: TransitionListenerHandler = DefaultTransitionListenerHandler()
+) : StateTransition, TransitionListenerHandler by handler {
     override fun start(state: State, listener: StateTransitionListener) {
         startTransition(this, addListenerAndRemoveOnEnd(listener))
     }
 
     override fun cancel() {
         onCancel()
-        transitionListenerHandler.dispatchTransitionCancel(this)
+        handler.dispatchTransitionCancel(this)
     }
 }
